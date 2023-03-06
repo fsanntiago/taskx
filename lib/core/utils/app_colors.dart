3a7dc47e3c6a7260a041ced43a488fa842629cf4
fsanntiago <zerofabricio@hotmail.com>
2023-03-05 22:30:35 -0300

    Initial commit

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color primaryColor = Color(0xff61b1fc);
+  static const Color successColor = Color(0xff4eF27D);
+  static const Color errorColor = Color(0xfff44336);
+
+  static const Color lightBottomBar = Color(0xffffffff);
+  static const Color lightBackgroundColor = Color(0xfffefefe);
+  static const Color lightShapeColor = Color(0xffffffff);
+  static const Color lightTitleColor = Color(0x00000000);
+  static const Color lightMainTextColor = Color(0xff757575);
+  static const Color lightTextSecondaryColor = Color(0xffababab);
+  static const Color lightBackgroundProgress = Color(0xffeeeeee);
+
+  static const Color lightAccentGreen = Color(0xffDCFEE4);
+  static const Color lightGreen = Color(0xff14FF00);
+  static const Color lightAccentRed = Color(0xffFEDCDC);
+  static const Color lightRed = Color(0xffFF0E00);
+  static const Color lightAccentPurple = Color(0xffEBDCFE);
+  static const Color lightPurple = Color(0xff5200FF);
+  static const Color lightAccentOrange = Color(0xffFEF2DC);
+  static const Color lightOrange = Color(0xffFF9900);
+
+  static const Color darkBottomBar = Color(0xff000000);
+  static const Color darkBackgroundColor = Color(0xff000132);
+  static const Color darkShapeColor = Color(0xff101010);
+  static const Color darkTitleColor = Color(0xffffffff);
+  static const Color darkMainTextColor = Color(0xffafafaf);
+  static const Color darkTextSecondaryColor = Color(0xffababab);
+  static const Color darkBackgroundProgress = Color(0xff333333);
+  static const Color darkBarProgress = Color(0xffffffff);
+  static const Color darkGreen = Color(0xff14FF00);
+  static const Color darkRed = Color(0xffFF0E00);
+  static const Color darkPurple = Color(0xff5200FF);
+  static const Color darkOrange = Color(0xffFF9900);
+}
