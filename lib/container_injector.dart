d4452eae4a996066c0742b45dcefedebc98eec8e
fsanntiago <zerofabricio@hotmail.com>
2023-04-28 12:43:34 -0300

    feat: env production injection

--- lib/container_injector.dart
+++ lib/container_injector.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:taskX/core/firebase/firestore_manager.dart';
 import 'package:taskX/core/network/connectivity_checker.dart';
@@ -41,35 +38,9 @@ void _initCore() {
   );
 }
 
-// Init firebase Emulator
 void _initFirebase() async {
-  var firebaseConfig = await _readJsonFile('firebase.json');
-  final firestorePort = firebaseConfig['emulators']['firestore']['port'] as int;
-  final authPort = firebaseConfig['emulators']['auth']['port'] as int;
-  final functionsPort = firebaseConfig['emulators']['functions']['port'] as int;
-  // final storagePort = firebaseConfig['emulators']['storage']['post'];
-
   // add firebase auth instance
-  sl.registerLazySingleton<FirebaseAuth>(() {
-    var authInstance = FirebaseAuth.instance;
-    // Use firestore emulator - localhost
-    authInstance.useAuthEmulator("127.0.0.1", 9099);
-
-    return authInstance;
-  });
-
+  sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   // add firebase firestore instance
-  sl.registerLazySingleton<FirebaseFirestore>(() {
-    var firestoreInstance = FirebaseFirestore.instance;
-    // Use firestore emulator - localhost
-    firestoreInstance.useFirestoreEmulator("127.0.0.1", 8080);
-    firestoreInstance.settings =
-        const Settings(sslEnabled: false, persistenceEnabled: false);
-    return firestoreInstance;
-  });
-}
-
-dynamic _readJsonFile(String filePath) async {
-  final String response = await rootBundle.loadString(filePath);
-  return await json.decode(response);
+  sl.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
 }
